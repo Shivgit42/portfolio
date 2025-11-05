6361b7fcfd264059abfb7814454c991acf851060
Shivam Rana <4826shivam@gmail.com>
2025-11-06 00:08:41 +0530

    refactor: enhance InitialLanding component layout with Title and TabbedProjectList for better user experience

--- src/app/(home)/components/InitialLanding.tsx
+++ src/app/(home)/components/InitialLanding.tsx
@@ -1,22 +1,25 @@
-"use client"
+"use client";
 
-import { motion } from "framer-motion"
-import HeroSection from "@/components/HeroSection"
-import ProjectCardList from "@/app/projects/components/ProjectCardList"
+import { motion } from "framer-motion";
+import HeroSection from "@/components/HeroSection";
+import TabbedProjectList from "@/components/CategorizedProjectList";
+import Title from "@/components/ui/Title";
 
 export default function InitialLanding() {
-    return (
-        <motion.div
-            initial={{ opacity: 0, y: 20 }}
-            animate={{ opacity: 1, y: 0 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.5 }}
-        >
-            <HeroSection />
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 20 }}
+      animate={{ opacity: 1, y: 0 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <HeroSection />
 
-            <div className="w-full flex justify-center mt-52 max-sm:mt-20">
-                <ProjectCardList />
-            </div>
-        </motion.div>
-    )
+      <div className="w-full flex flex-col justify-center items-center mt-52 max-sm:mt-20">
+        <Title title="Proof of Work" />
+        <br />
+        <TabbedProjectList />
+      </div>
+    </motion.div>
+  );
 }
