08ffeef6a85c7f5997559a62edc554180aa7717e
Shivam Rana <4826shivam@gmail.com>
2025-11-06 14:22:52 +0530

    refactor: update Tabs component styles for active state to improve visual clarity

--- src/components/CategorizedProjectList.tsx
+++ src/components/CategorizedProjectList.tsx
@@ -11,28 +11,28 @@ const CategorizedProjectList = () => {
     <div className="w-full">
       <Tabs defaultValue="full stack apps" className="w-full">
         <div className="flex justify-center mb-8 max-sm:mb-4 max-sm:px-2">
-          <TabsList className="w-full max-w-xl max-sm:max-w-4xl bg-white bg-opacity-10 backdrop-blur-lg border dark:border-white/10 grid grid-cols-1 sm:grid-cols-4 gap-1">
+          <TabsList className="w-full max-w-3xl sm:max-w-4xl bg-white bg-opacity-10 backdrop-blur-lg border dark:border-white/10 grid grid-cols-1 sm:grid-cols-4 gap-1">
             <TabsTrigger
               value="full stack apps"
-              className="text-xs sm:text-sm data-[state=active]:bg-black/50"
+              className="text-xs sm:text-sm data-[state=active]:bg-white/20"
             >
               Full Stack
             </TabsTrigger>
             <TabsTrigger
               value="client projects"
-              className="text-xs sm:text-sm data-[state=active]:bg-black/50"
+              className="text-xs sm:text-sm data-[state=active]:bg-white/20"
             >
               Client Projects
             </TabsTrigger>
             <TabsTrigger
               value="extensions"
-              className="text-xs sm:text-sm data-[state=active]:bg-black/50"
+              className="text-xs sm:text-sm data-[state=active]:bg-white/20"
             >
               Chrome Extensions
             </TabsTrigger>
             <TabsTrigger
               value="learning projects"
-              className="text-xs sm:text-sm data-[state=active]:bg-black/50"
+              className="text-xs sm:text-sm data-[state=active]:bg-white/20"
             >
               Learning Platform
             </TabsTrigger>
