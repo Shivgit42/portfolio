7021edfb4d239ee590fed6dbc47dd05b346c45c3
Shivam Rana <shivam105228@gmail.com>
2026-02-09 00:22:35 +0530

    feat: Introduce Prisma with Blog and User models, add an Avatar component and image assets, and update the build script.

--- src/app/(home)/components/Avatar.tsx
+++ src/app/(home)/components/Avatar.tsx
@@ -7,7 +7,7 @@ const Avatar = () => {
       href="https://x.com/intent/follow?screen_name=shivamrtwt"
       target="_blank"
     >
-      <Picture src="/spidey.jpeg" fallback="A" size="8" radius="full" />
+      <Picture src="/sr.png" fallback="A" size="8" radius="full" />
     </Link>
   );
 };
